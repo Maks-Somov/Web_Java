e3fbee9dbb27d5be93d70e2d91b9399371ab7c55
Maks-Somov <fail.epic2015@yandex.ru>
2020-02-04 19:42:02 +0300

    создали пакет, который не будет относиться к приложению, а только для примера

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,33 +6,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        Scanner sc = new Scanner(System.in);
-        int Arr[] = new int[10];
-        for(int i=0;i<Arr.length;i++){
-            Arr[i]=(int)(Math.random()*100);
-            System.out.println(i+" : "+Arr[i]);
-        }
-        System.out.println("  Hello maks\n"+"if you want find min press 1.\n"+"if max - press 2");
-        int number = sc.nextInt();
-        int min = Arr[0];
-        int max = Arr[0];
-        switch (number){
-            case 1: for(int i=0;i<Arr.length;i++){
-            if(min>Arr[i]){
-                min = Arr[i];
-                    }
-                }
-        System.out.println(min);
-            break;
-            case 2: for(int i=0;i<Arr.length;i++){
-            if(max<Arr[i]){
-                max = Arr[i];
-                    }
-                }
-        System.out.println(max);
-        break;
-            default:
-System.out.println("incorrect number!!");
-        }
+
     }
 }
