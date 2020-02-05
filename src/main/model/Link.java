082bf03c1d42dc77390ffad5ca1a44552fab5f7a
Maks-Somov <fail.epic2015@yandex.ru>
2020-02-05 12:21:27 +0300

    add Link

---
+++ src/main/model/Link.java
@@ -0,0 +1,11 @@
+package main.model;
+
+/**
+ * Maks
+ * 05.02.2020.
+ */
+public class Link {
+    private String name;
+    private String url;
+
+}
---
+++ src/main/model/Resume.java
@@ -0,0 +1,9 @@
+package main.model;
+
+import java.util.Scanner;
+
+public class Resume {
+    private String fullName;
+    private String location;
+
+}
