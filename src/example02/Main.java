d5b539b10f228e496a4909d70736e605f8c627a6
Maks-Somov <fail.epic2015@yandex.ru>
2020-02-05 15:11:23 +0300

    Main

--- src/example02/Main.java
+++ src/example02/Main.java
@@ -1,8 +1,14 @@
 package example02;
 
+import main.model.Link;
+
 /**
  * Maks
  * 05.02.2020.
  */
 public class Main {
+    public static void main(String args[]){
+        Link link = new Link("Maks", "github.com");
+        System.out.println(link);
+    }
 }
--- src/main/model/Link.java
+++ src/main/model/Link.java
@@ -5,11 +5,16 @@ package main.model;
  * 05.02.2020.
  */
 public class Link {
-    private String name;
-    private String url;
+    private final String name;
+    private final String url;
 
     public Link(String name, String url) {
         this.name = name;
         this.url = url;
+    }
+
+    public Link(Link link) {
+        this.name = link.name;
+        this.url = link.url;
     }
 }
