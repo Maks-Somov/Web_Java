00bed9065bcd31c924b5219a2651b9950694a1ef
Maks-Somov <fail.epic2015@yandex.ru>
2020-02-06 13:00:19 +0300

    Enhance SectionType

--- src/main/model/SectionType.java
+++ src/main/model/SectionType.java
@@ -5,9 +5,19 @@ package main.model;
  * 06.02.2020.
  */
 public enum SectionType {
-    OBJECTIVE,
-    ACHIEVEMENT,
-    QUALIFICATIONS,
-    EXPERIENCE,
-    EDUCATION
+    OBJECTIVE("Позиция"),
+    ACHIEVEMENT("Достижения"),
+    QUALIFICATIONS("Квалификация"),
+    EXPERIENCE("Опыт работы"),
+    EDUCATION("Образование");
+
+
+private String tittle;
+
+    SectionType(String tittle) {
+        this.tittle = tittle;
+    }
+    public String getTittle() {
+        return tittle;
+    }
 }
