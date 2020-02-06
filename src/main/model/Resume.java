d6aa2ffad6e9308d7b75dcecd159970cb1eb52bc
Maks-Somov <fail.epic2015@yandex.ru>
2020-02-06 15:37:10 +0300

    add resume constructor

--- src/main/model/Resume.java
+++ src/main/model/Resume.java
@@ -1,17 +1,21 @@
 package main.model;
 
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Resume {
     private String uuid;
     private String fullName;
     private String location;
     private String homePage;
-    private List<Contact> contacts;
-    private List<Section> sections;
+    private List<Contact> contacts = new LinkedList<>();
+    private List<Section> sections = new LinkedList<>();
 
     public Resume(String fullName, String location) {
+        this(UUID.randomUUID().toString(), fullName,location);
+    }
+
+    public Resume(String uuid, String fullName, String location) {
+        this.uuid = uuid;
         this.fullName = fullName;
         this.location = location;
     }
@@ -29,5 +33,38 @@ public class Resume {
     @Override
     public int hashCode() {
         return uuid.hashCode();
+    }
+
+    public void addSection(Section section){
+        sections.add(section);
+    }
+
+    public void addContact(Contact contact){
+        contacts.add(contact);
+    }
+
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public String getHomePage() {
+        return homePage;
+    }
+
+    public List<Contact> getContacts() {
+        return contacts;
+    }
+
+    public List<Section> getSections() {
+        return sections;
     }
 }
