83a0522b8b13e706f04945f5913c5ef64348d459
Maks-Somov <fail.epic2015@yandex.ru>
2020-02-06 13:28:43 +0300

    Reflection sample

--- src/example03/Main.java
+++ src/example03/Main.java
@@ -2,13 +2,26 @@ package example03;
 
 import main.model.Contact;
 import main.model.ContactType;
+import main.model.Link;
+
+import java.lang.reflect.Field;
 
 /**
  * Maks
  * 06.02.2020.
  */
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoSuchFieldException, IllegalAccessException {
         Contact c = new Contact(ContactType.PHONE, "+375296666666");
+
+        Field field = Link.class.getDeclaredField("url");
+        field.setAccessible(true);
+        Link link = new Link("sdfs", "URL");
+        System.out.println(field.get(link));
+        System.out.println(link.getUrl());
     }
+
+
+
+
 }
