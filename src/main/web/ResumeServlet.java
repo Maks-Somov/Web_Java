fde8583ef92062e6f84a2d216dbd6483a51fd341
Maks-Somov <fail.epic2015@yandex.ru>
2020-02-19 22:11:09 +0300

    create class servlet

--- src/main/web/ResumeServlet.java
+++ src/main/web/ResumeServlet.java
@@ -1,13 +1,21 @@
 package main.web;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.Writer;
 
 public class ResumeServlet extends javax.servlet.http.HttpServlet {
-    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws javax.servlet.ServletException, IOException {
 
     }
 
-    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
-
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html; charset=UTF-8");
+        Writer w = response.getWriter();
+        String name = request.getParameter("name");
+        w.write("Текс сервлет: привет "+ name);
+        w.close();
     }
 }
